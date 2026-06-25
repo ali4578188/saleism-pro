0b97b96f7b720c16131a8bd4d1841d87e39fe81c
ali4578188 <zohaib.rlink@gmail.com>
2026-06-25 21:01:53 +0500

    Add lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'app.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Lock portrait orientation
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  // Dark status bar
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.light,
+    systemNavigationBarColor: Color(0xFF0A0A0A),
+    systemNavigationBarIconBrightness: Brightness.light,
+  ));
+
+  runApp(const SaleismApp());
+}
